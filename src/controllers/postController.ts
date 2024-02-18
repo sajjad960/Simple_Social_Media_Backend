45a43a870bb3cc3f0b2e0867c16878234805fa46
Sajjad <sajjadhossainsb01@gmail.com>
2024-02-18 20:25:47 +0600

    got the image buffer

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -3,6 +3,7 @@ import factory from "./handleFactory";
 import catchAsync from "../utils/catchAsync";
 import Post from "../models/postModel";
 import path from "path";
+import fs from "fs/promises";
 
 // const createUser = factory.createOne(Users);
 // const getUsers = factory.getAll(Users);
@@ -12,17 +13,46 @@ interface imageObj {}
 const createPost = catchAsync(async (req: any, res: Response) => {
   const { text } = req.body;
   const images = req?.files?.images;
-  const imagesName = [];
-  const userId = req?.user?.id
+  const imagesNames = [];
+  const userId = req?.user?.id;
 
-  images?.map((e: any, i: number) => {
-    imagesName.push(e?.filename);
-  });
+  console.log(images);
 
+  await Promise.all(images?.map(async (e: any, i: number) => {
+    const destinationPath = path.join(
+      __dirname,
+      "../files/image",
+      String(userId)
+    );
+
+    try {
+      // Check if the folder already exists
+      await fs.access(destinationPath);
+    } catch (error) {
+      // If the folder doesn't exist, create it
+      try {
+        await fs.mkdir(destinationPath, { recursive: true });
+      } catch (err) {
+        console.error("Error creating folder:", err);
+      }
+    }
+
+    const newFileName = Date.now() + "-" + e.originalname;
+    const filePath = path.join(destinationPath, newFileName);
+    await fs.writeFile(filePath, e.buffer);
+
+    console.log(filePath);
+
+    imagesNames.push(newFileName);
+    console.log("read execute into");
+
+  }));
+  
+  console.log("read execute after");
   // Create the post
   const post = await Post.create({
     text,
-    images: JSON.stringify(imagesName),
+    images: JSON.stringify(imagesNames),
     user_id: userId
   });
 
@@ -31,19 +61,23 @@ const createPost = catchAsync(async (req: any, res: Response) => {
 
 const getUploadImages = catchAsync(async (req: any, res: Response) => {
   const userid = req.params.id;
-  const imagename = req.params.imagename
-  const filePath = path.join(__dirname, '../files/image', String(userid), imagename);
+  const imagename = req.params.imagename;
+  const filePath = path.join(
+    __dirname,
+    "../files/image",
+    String(userid),
+    imagename
+  );
 
   res.sendFile(filePath, (err) => {
     if (err) {
-      console.error('Error sending file:', err);
-      res.status(404).send('File not found');
+      console.error("Error sending file:", err);
+      res.status(404).send("File not found");
     }
   });
+});
 
-})
-
-const getAllPosts = factory.getAll(Post)
+const getAllPosts = factory.getAll(Post);
 // Export
 const postController = { createPost, getAllPosts, getUploadImages };
 export = postController;
--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -3,35 +3,37 @@ import multer from "multer";
 import path from "path";
 import fs from "fs/promises";
 
-const storage = multer.diskStorage({
-  destination: async function (req: any, file, cb) {
-    if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
-      const userId: string = String(req.user.id);
+// const storage = multer.diskStorage({
+//   destination: async function (req: any, file, cb) {
+//     if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+//       const userId: string = String(req.user.id);
 
-      const destinationPath = path.join(__dirname, "../files/image", userId);
+//       const destinationPath = path.join(__dirname, "../files/image", userId);
 
-      try {
-        // Check if the folder already exists
-        await fs.access(destinationPath);
-      } catch (error) {
-        // If the folder doesn't exist, create it
-        try {
-          await fs.mkdir(destinationPath, { recursive: true });
-        } catch (err) {
-          console.error("Error creating folder:", err);
-        }
-      }
+//       try {
+//         // Check if the folder already exists
+//         await fs.access(destinationPath);
+//       } catch (error) {
+//         // If the folder doesn't exist, create it
+//         try {
+//           await fs.mkdir(destinationPath, { recursive: true });
+//         } catch (err) {
+//           console.error("Error creating folder:", err);
+//         }
+//       }
 
-      cb(null, destinationPath);
-    } else {
-      cb(null, path.join(__dirname, "../public/document"));
-    }
-  },
-  filename: function (req, file, cb) {
-    const name = Date.now() + "-" + file.originalname;
-    cb(null, name);
-  },
-});
+//       cb(null, destinationPath);
+//     } else {
+//       cb(null, path.join(__dirname, "../public/document"));
+//     }
+//   },
+//   filename: function (req, file, cb) {
+//     const name = Date.now() + "-" + file.originalname;
+//     cb(null, name);
+//   },
+// });
+
+const storage = multer.memoryStorage();
 
 const fileFilter = (req, file, cb) => {
   if (file.fieldname === "images") {
