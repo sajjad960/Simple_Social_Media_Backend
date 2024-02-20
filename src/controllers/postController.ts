49c14320266be65e0af5a5c0367f4f1a6af593be
Sajjad <sajjadhossainsb01@gmail.com>
2024-02-20 18:58:33 +0600

    need to optimized tensorflow image validation code

--- src/controllers/postController.ts
+++ src/controllers/postController.ts
@@ -4,11 +4,33 @@ import catchAsync from "../utils/catchAsync";
 import Post from "../models/postModel";
 import path from "path";
 import fs from "fs/promises";
+import { load } from "@tensorflow-models/coco-ssd";
+import { node } from "@tensorflow/tfjs-node";
+import Jimp from "jimp";
 
 // const createUser = factory.createOne(Users);
 // const getUsers = factory.getAll(Users);
 
 interface imageObj {}
+
+// Load COCO-SSD model
+// let model: any;
+// async function loadModel() {
+// }
+// loadModel();
+
+// Function to run object detection on the image
+async function detectObjects(imageBuffer) {
+  try {
+    const model = await load();
+    const image = node.decodeImage(imageBuffer);
+    const predictions = await model.detect(image);
+
+    return predictions;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 const createPost = catchAsync(async (req: any, res: Response) => {
   const { text } = req.body;
@@ -17,40 +39,66 @@ const createPost = catchAsync(async (req: any, res: Response) => {
   const userId = req?.user?.id;
 
   // create files into directory
-  await Promise.all(images?.map(async (e: any, i: number) => {
-    const destinationPath = path.join(
-      __dirname,
-      "../files/image",
-      String(userId)
-    );
+  await Promise.all(
+    images?.map(async (e: any, i: number) => {
+      const destinationPath = path.join(
+        __dirname,
+        "../files/image",
+        String(userId)
+      );
 
-    try {
-      // Check if the folder already exists
-      await fs.access(destinationPath);
-    } catch (error) {
-      // If the folder doesn't exist, create it
       try {
-        await fs.mkdir(destinationPath, { recursive: true });
-      } catch (err) {
-        console.error("Error creating folder:", err);
+        // Check if the folder already exists
+        await fs.access(destinationPath);
+      } catch (error) {
+        // If the folder doesn't exist, create it
+        try {
+          await fs.mkdir(destinationPath, { recursive: true });
+        } catch (err) {
+          console.error("Error creating folder:", err);
+        }
       }
-    }
+      console.log("hello");
 
-    const newFileName = Date.now() + "-" + e.originalname;
-    const filePath = path.join(destinationPath, newFileName);
-    await fs.writeFile(filePath, e.buffer);
+      // const newbuffer = await Jimp.read(e.buffer)
+      //   .then((image) => {
+      //     // Resize the image
+      //     return image.resize(320, 192);
+      //   })
+      //   .then((resizedImage) => {
+      //     // Get the resized image as a buffer
+      //     return resizedImage.getBufferAsync("image/jpeg");
+      //   })
+      //   .then((buffer) => {
+      //     return buffer;
+      //   })
+      //   .catch((err) => {
+      //     console.log(err);
+      //     res.status(500).send("Error resizing image.");
+      //   });
 
-    imagesNames.push(newFileName);
-  }));
-  
+      // const predictions = await detectObjects(newbuffer);
+
+      // console.log("production", predictions);
+
+      const newFileName = Date.now() + "-" + e.originalname;
+      const filePath = path.join(destinationPath, newFileName);
+      await fs.writeFile(filePath, e.buffer);
+
+      imagesNames.push(newFileName);
+    })
+  );
+
   // Create the post
   const post = await Post.create({
     text,
     images: JSON.stringify(imagesNames),
-    user_id: userId
+    user_id: userId,
   });
 
-  res.status(201).json({ success: true, message: "Post created successfully", post });
+  res
+    .status(201)
+    .json({ success: true, message: "Post created successfully", post });
 });
 
 const getUploadImages = catchAsync(async (req: any, res: Response) => {
