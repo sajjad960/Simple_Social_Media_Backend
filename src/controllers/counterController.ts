b8394413f6d617ce77ea811c1aade4ee3e28d9a7
Sajjad <sajjadhossainsb01@gmail.com>
2024-03-01 10:45:28 +0600

    counter increment and creation api implemented

--- src/controllers/counterController.ts
+++ src/controllers/counterController.ts
@@ -3,13 +3,34 @@ import catchAsync from "../utils/catchAsync";
 import Counter from "../models/counterModel";
 import Replies from "../models/repliesModel";
 import AppError from "../utils/AppError";
+import Post from "../models/postModel";
+import Commment from "../models/commentModel";
+import { sequelize } from "../instances/sequelize";
 
 const newReact = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const { reactName, type, id } = req.body;
+    // type === "post" && Post || type === "comment" && Commment || type === "reply" && Replies
 
     async function createOrIncrementReact(type, idTypeName, id) {
-      const isReactExist = await Replies.findOne({
+      // Check Id Is Still Valid
+      const model =
+        (type === "post" && Post) ||
+        (type === "comment" && Commment) ||
+        (type === "reply" && Replies);
+
+      const isIdValid = await model.findOne({
+        where: {
+          id,
+        },
+      });
+
+      if (!isIdValid) {
+        return next(new AppError(`${type.charAt(0).toUpperCase() + type.slice(1)} not found`, 404));
+      }
+
+      // Check If React Already Created For The Type Or Not.
+      const isReactExist = await Counter.findOne({
         where: {
           [idTypeName]: id,
         },
@@ -23,12 +44,13 @@ const newReact = catchAsync(
         const newCounterEntry = await Counter.create({
           type,
           [idTypeName]: id,
+          [reactName]: 1,
         });
       }
-      
+
       res.status(200).json({
         status: "success",
-        message: "Reaction Added"
+        message: "Reaction Added",
       });
     }
 
